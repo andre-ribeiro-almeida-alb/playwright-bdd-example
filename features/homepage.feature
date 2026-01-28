a3ee2df7f944fc3462b4d17e638e24cfd6b83e67
andre-ribeiro-almeida-alb <125457358+andre-ribeiro-almeida-alb@users.noreply.github.com>
2026-01-28 12:50:37 +0000

    Add reproducible example for step matching

--- features/homepage.feature
+++ features/homepage.feature
@@ -4,3 +4,7 @@ Feature: Playwright Home Page
     Given I am on Playwright home page
     When I click link "Get started"
     Then I see in title "Installation"
+    And This step should be matched with slash
+    And This step should be matched with quote "some quote" with slash
+    And This step should be matched with quote "some quote" without slash
+    And This step should be matched with unquoted word potato with slash
--- features/steps/index.ts
+++ features/steps/index.ts
@@ -12,3 +12,19 @@ When('I click link {string}', async ({ page }, name: string) => {
 Then('I see in title {string}', async ({ page }, text: string) => {
   await expect(page).toHaveTitle(new RegExp(text));
 });
+
+Then('This step should/texts be matched with slash', async ({ page }) => {
+  await expect(page).toHaveURL(/.*playwright\.dev.*/);
+});
+
+Then('This step should/texts be matched with quote {string} with slash', async ({ page }) => {
+  await expect(page).toHaveURL(/.*playwright\.dev.*/);
+});
+
+Then('This step should be matched with quote {string} without slash', async ({ page }) => {
+  await expect(page).toHaveURL(/.*playwright\.dev.*/);
+});
+
+Then('This step should/texts be matched with unquoted word {word} with slash', async ({ page }) => {
+  await expect(page).toHaveURL(/.*playwright\.dev.*/);
+});
