a4ee9721456f529c13ab9a33f27b541660ce5f3d
andre-ribeiro-almeida-alb <andre-ribeiro-almeida@alticelabs.com>
2026-03-25 18:21:16 +0100

    Remove steps definition from index.ts

--- features/steps/index.ts
+++ features/steps/index.ts
@@ -12,23 +12,3 @@ When('I click link {string}', async ({ page }, name: string) => {
 Then('I see in title {string}', async ({ page }, text: string) => {
   await expect(page).toHaveTitle(new RegExp(text));
 });
-
-Given('I am logged in TSTs env', async ({}) => {
-  // Step: Given I am logged in TSTs env
-  // From: features\homepage.feature:14:5
-});
-
-When('I open the projects module', async ({}) => {
-  // Step: When I open the projects module
-  // From: features\homepage.feature:15:5
-});
-
-When('I search project with name {string}', async ({}, arg: string) => {
-  // Step: And I search project with name "Teste Projecto Com Entidades"
-  // From: features\homepage.feature:16:5
-});
-
-Then('I validate I see the project with a simple expect', async ({}) => {
-  // Step: Then I validate I see the project with a simple expect
-  // From: features\homepage.feature:17:5
-});
